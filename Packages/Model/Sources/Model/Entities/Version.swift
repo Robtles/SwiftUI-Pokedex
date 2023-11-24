a18c2c575b1d93487cc412d4bd61a973a4704367
Rob <robbeyroad@gmail.com>
2023-11-24 10:52:29 +0100

    Added simpler access to version indexes in model

--- Packages/Model/Sources/Model/Entities/Version.swift
+++ Packages/Model/Sources/Model/Entities/Version.swift
@@ -13,7 +13,7 @@ public struct Version {
     /// The constants related to the Version model
     public enum Constants {
         /// The valid Pokémon game versions (from the 4 first generations)
-        public static let validVersions = (1...15).map { $0 } + [24, 25]
+        public static let validVersions = (red...soulSilver).map { $0 } + [omegaRuby, alphaSapphire]
     }
     
     // MARK: Properties
@@ -36,4 +36,27 @@ extension Version: Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(id.id)
     }
+}
+
+// MARK: - Version Index
+/// List of all the versions with their corresponding index from PokéAPI
+public extension Version {
+    static let red = 1
+    static let blue = 2
+    static let yellow = 3
+    static let gold = 4
+    static let silver = 5
+    static let crystal = 6
+    static let ruby = 7
+    static let sapphire = 8
+    static let emerald = 9
+    static let fireRed = 10
+    static let leafGreen = 11
+    static let diamond = 12
+    static let pearl = 13
+    static let platinum = 14
+    static let heartGold = 15
+    static let soulSilver = 16
+    static let omegaRuby = 25
+    static let alphaSapphire = 26
 }
