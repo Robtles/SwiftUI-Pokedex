4937f31fa7ba44f1989b2dc11530d395de19c9ce
Rob <robbeyroad@gmail.com>
2023-12-09 20:49:10 +0100

    Added ability to go up or down 50 elements in the list when pushing the left or right button of the tvOS remote

--- Packages/UI/Sources/UI/Views/PokemonList/PokemonListView.swift
+++ Packages/UI/Sources/UI/Views/PokemonList/PokemonListView.swift
@@ -12,8 +12,16 @@ import SwiftUI
 // MARK: - Pokemon List View
 /// The initial Pokémon list view
 public struct PokemonListView: View {
+    // MARK: Static Properties
+    private enum Constants {
+        fileprivate static let advanceThreshold = 50
+    }
+    
     // MARK: Type Properties
     private typealias PokemonListViewContent = (key: Int, value: LocalizedContentDictionary)
+    
+    // MARK: State Properties
+    @FocusState private var focused: Int?
     
     // MARK: Environment Properties
     @Environment(\.colorScheme) fileprivate var colorScheme
@@ -38,32 +46,72 @@ public struct PokemonListView: View {
     
     // MARK: View Properties
     public var body: some View {
-        List(sortedPokemons, id: \.key) {
-            PokemonListRowView(
-                id: $0.key,
-                localizedNames: $0.value
+        ScrollViewReader { proxy in
+            List(sortedPokemons, id: \.key) {
+                PokemonListRowView(
+                    id: $0.key,
+                    localizedNames: $0.value
+                )
+                .focused($focused, equals: $0.key)
+                .listRowInsets(EdgeInsets())
+                #if !os(tvOS)
+                .listRowSeparatorTint(
+                    Colors.secondaryBackground.from(defaults, colorScheme: colorScheme)
+                )
+                #endif
+                #if os(macOS)
+                // ugly workaround for macOS to remove unnecessary horizontal padding around the list rows
+                .padding(.horizontal, -8.0)
+                #endif
+            }
+            .background(
+                Colors.primaryBackground.from(defaults, colorScheme: colorScheme)
             )
-            .listRowInsets(EdgeInsets())
-            #if !os(tvOS)
-            .listRowSeparatorTint(
-                Colors.secondaryBackground.from(defaults, colorScheme: colorScheme)
-            )
-            #endif
-            #if os(macOS)
-            // ugly workaround for macOS to remove unnecessary horizontal padding around the list rows
-            .padding(.horizontal, -8.0)
+            .listStyle(PlainListStyle())
+            #if os(tvOS)
+            .onMoveCommand(perform: handleMoveCommand(proxy: proxy))
             #endif
         }
-        .background(
-            Colors.primaryBackground.from(defaults, colorScheme: colorScheme)
-        )
-        .listStyle(PlainListStyle())
     }
     
     // MARK: Init Methods
-    public init(pokemons: [Int : LocalizedContentDictionary]) {
+    public init(pokemons: [Int: LocalizedContentDictionary]) {
         self.pokemons = pokemons
     }
+    
+    // MARK: View Methods
+    #if os(tvOS)
+    private enum ListAdvanceDirection {
+        case up, down
+    }
+    
+    private func handleMoveCommand(proxy: ScrollViewProxy) -> (MoveCommandDirection) -> Void {
+        return { direction in
+            switch direction {
+            case .left:
+                advanceInList(.up, proxy)
+            case .right:
+                advanceInList(.down, proxy)
+            default:
+                return
+            }
+        }
+    }
+    
+    private func advanceInList(
+        _ direction: ListAdvanceDirection, 
+        _ proxy: ScrollViewProxy
+    ) {
+        withAnimation {
+            let newFocus = switch direction {
+            case .down: min(Pokemon.Constants.maximumId, (focused ?? Pokemon.Constants.minimumId) + Constants.advanceThreshold)
+            case .up: max(Pokemon.Constants.minimumId, (focused ?? Pokemon.Constants.minimumId) - Constants.advanceThreshold)
+            }
+            focused = newFocus
+            proxy.scrollTo(newFocus, anchor: .top)
+        }
+    }
+    #endif
 }
 
 struct PokemonListViewPreview: PreviewProvider {
