baf591045f4017769828b15e906d8a37ec1bb2ab
Rob <robbeyroad@gmail.com>
2024-01-02 10:38:49 +0100

    Added Pokémon Header view

---
+++ Packages/UI/Sources/UI/Views/PokemonHeaderView/Platform/PokemonHeaderView~iPhone.swift
@@ -0,0 +1,78 @@
+//
+//  PokemonHeaderView~iPhone.swift
+//
+//  Created by Rob on 16/12/2023.
+//
+
+import Defaults
+import Mock
+import Model
+import SwiftUI
+
+// MARK: - Pokemon Header View (iPhone)
+struct PokemonHeaderView_iPhone: View {
+    // MARK: Constants
+    fileprivate enum Constants {
+        fileprivate static let contentStackSpacing = 8.0
+        fileprivate static let emptyPokemonName = "--"
+        fileprivate static let height = 76.0
+        fileprivate static let mainStackSpacing = 12.0
+    }
+    
+    // MARK: Environment Properties
+    @Environment(Defaults.self) private var defaults
+    
+    // MARK: Instance Properties
+    let names: LocalizedContentDictionary
+    let pokemon: Pokemon
+    
+    // MARK: View Properties
+    var body: some View {
+        HStack(spacing: Constants.mainStackSpacing) {
+            ImageView(
+                url: URLBuilder.shared.url(
+                    for: .pokemonImage,
+                    pokemonId: pokemon.id.id
+                )
+            )
+            VStack(spacing: Constants.contentStackSpacing) {
+                HStack {
+                    Text(names[defaults.language] ?? Constants.emptyPokemonName)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text(
+                        "(" +
+                        "\(pokemon.id.id.formatted(withDigits: 3))" +
+                        ")"
+                    )
+                    .font(.subheadline)
+                    .fontWeight(.light)
+                    Spacer()
+                }
+                PokemonBadgeTypeContainerView(
+                    pokemonTypes: pokemon.types
+                )
+            }
+            Spacer()
+        }
+        .frame(height: Constants.height)
+        .padding()
+    }
+}
+
+struct PokemonHeaderView_iPhonePreview: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PokemonHeaderView_iPhone(
+                names: pikachuLocalizedNames,
+                pokemon: pikachuPokemon
+            )
+            .preview(in: .iOS, displayMode: .light)
+            PokemonHeaderView_iPhone(
+                names: pikachuLocalizedNames,
+                pokemon: pikachuPokemon
+            )
+            .preview(in: .iOS, displayMode: .dark)
+        }
+    }
+}
---
+++ Packages/UI/Sources/UI/Views/PokemonHeaderView/PokemonHeaderView.swift
@@ -0,0 +1,53 @@
+//
+//  PokemonHeaderView.swift
+//
+//  Created by Rob on 16/12/2023.
+//
+
+import Mock
+import Model
+import SwiftUI
+
+// MARK: - Pokemon Header View
+struct PokemonHeaderView: View {
+    // MARK: Instance Properties
+    let names: LocalizedContentDictionary
+    let pokemon: Pokemon
+    
+    // MARK: View Properties
+    var body: some View {
+        buildPokemonHeaderView()
+    }
+    
+    // MARK: View Methods
+    @ViewBuilder private func buildPokemonHeaderView() -> some View {
+        switch Platform.current {
+        case .iOS:
+            PokemonHeaderView_iPhone(
+                names: names,
+                pokemon: pokemon
+            )
+        default:
+            EmptyView()
+        }
+    }
+}
+
+struct PokemonHeaderViewPreview: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ForEach(Platform.allCases, id: \.self) { platform in
+                PokemonHeaderView(
+                    names: pikachuLocalizedNames,
+                    pokemon: pikachuPokemon
+                )
+                .preview(in: platform, displayMode: .light)
+                PokemonHeaderView(
+                    names: pikachuLocalizedNames,
+                    pokemon: pikachuPokemon
+                )
+                .preview(in: platform, displayMode: .dark)
+            }
+        }
+    }
+}
