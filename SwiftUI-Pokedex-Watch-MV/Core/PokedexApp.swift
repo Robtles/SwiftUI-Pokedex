948a4532952c444205f80abde5647ffa0783dc34
Rob <robbeyroad@gmail.com>
2024-01-02 22:59:15 +0100

    Added assets for MV target and watch target

---
+++ SwiftUI-Pokedex-Watch-MV/Core/PokedexApp.swift
@@ -0,0 +1,19 @@
+//
+//  PokedexApp.swift
+//  SwiftUI-Pokedex-Watch-MV
+//
+//  Created by Rob on 02/01/2024.
+//
+
+import SwiftUI
+
+// MARK: - App
+@main
+struct PokedexApp: App {
+    // MARK: View Properties
+    var body: some Scene {
+        WindowGroup {
+            Color.red
+        }
+    }
+}
