d5b0816f05f1b347b536165a896111a3d6766578
Rob <robbeyroad@gmail.com>
2024-01-02 11:32:17 +0100

    Added carriage return removal extension

--- Packages/UI/Sources/UI/Extensions/String+Extension.swift
+++ Packages/UI/Sources/UI/Extensions/String+Extension.swift
@@ -7,6 +7,10 @@
 import Foundation
 
 public extension String {
+    var carriageReturnFixed: String {
+        return replacingOccurrences(of: "\n", with: " ")
+    }
+    
     var localized: String {
         return NSLocalizedString(self, comment: "")
     }
