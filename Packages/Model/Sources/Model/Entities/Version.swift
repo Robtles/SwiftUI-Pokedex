75910f63bd781bb1bb29b4e6817fb85db4fcac39
Rob <robbeyroad@gmail.com>
2024-01-02 17:36:46 +0100

    Added missing version names

--- Packages/Model/Sources/Model/Entities/Version.swift
+++ Packages/Model/Sources/Model/Entities/Version.swift
@@ -57,6 +57,34 @@ public extension Version {
     static let platinum = 14
     static let heartGold = 15
     static let soulSilver = 16
+    
+    static let black = 17
+    static let white = 18
+    static let colosseum = 19
+    static let xd = 20
+    static let black2 = 21
+    static let white2 = 22
+    static let x = 23
+    static let y = 24
+    
     static let omegaRuby = 25
     static let alphaSapphire = 26
+
+    static let sun = 27
+    static let moon = 28
+    static let ultraSun = 29
+    static let ultraMoon = 30
+    static let letsGoPikachu = 31
+    static let letsGoEevee = 32
+    static let sword = 33
+    static let shield = 34
+    static let theIsleOfArmor = 35
+    static let theCrownTundra = 36
+    static let brilliantDiamond = 37
+    static let shiningPearl = 38
+    static let legendsArceus = 39
+    static let scarlet = 40
+    static let violet = 41
+    static let theTealMask = 42
+    static let theIndigoDisk = 43
 }
--- Packages/UI/Sources/UI/Extensions/Version+Extension.swift
+++ Packages/UI/Sources/UI/Extensions/Version+Extension.swift
@@ -27,8 +27,33 @@ extension VersionID {
         case Version.platinum: Strings.Version.platinum
         case Version.heartGold: Strings.Version.heartgold
         case Version.soulSilver: Strings.Version.soulsilver
+        case Version.black: Strings.Version.black
+        case Version.white: Strings.Version.white
+        case Version.colosseum: Strings.Version.colosseum
+        case Version.xd: Strings.Version.xd
+        case Version.black2: Strings.Version.black2
+        case Version.white2: Strings.Version.white2
+        case Version.x: Strings.Version.x
+        case Version.y: Strings.Version.y
         case Version.omegaRuby: Strings.Version.omegaruby
-        case Version.alphaSapphire: Strings.Version.alphasapphire
+        case Version.alphaSapphire: Strings.Version.alphasapphire            
+        case Version.sun: Strings.Version.sun
+        case Version.moon: Strings.Version.moon
+        case Version.ultraSun: Strings.Version.ultraSun
+        case Version.ultraMoon: Strings.Version.ultraMoon
+        case Version.letsGoPikachu: Strings.Version.letsGoPikachu
+        case Version.letsGoEevee: Strings.Version.letsGoEevee
+        case Version.sword: Strings.Version.sword
+        case Version.shield: Strings.Version.shield
+        case Version.theIsleOfArmor: Strings.Version.theIsleOfArmor
+        case Version.theCrownTundra: Strings.Version.theCrownTundra
+        case Version.brilliantDiamond: Strings.Version.brilliantDiamond
+        case Version.shiningPearl: Strings.Version.shiningPearl
+        case Version.legendsArceus: Strings.Version.legendsArceus
+        case Version.scarlet: Strings.Version.scarlet
+        case Version.violet: Strings.Version.violet
+        case Version.theTealMask: Strings.Version.theTealMask
+        case Version.theIndigoDisk: Strings.Version.theIndigoDisk
         default: nil
         }
     }
